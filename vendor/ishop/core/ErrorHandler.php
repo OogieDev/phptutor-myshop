98cd66bbe2133c39ee6ca0717ce4111f27a56bdc
Oogie <oogie.webdev@gmail.com>
2018-07-07 02:35:01 +0300

    lesson5 errors

---
+++ vendor/ishop/core/ErrorHandler.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace ishop;
+
+
+class ErrorHandler
+{
+
+    public function __construct()
+    {
+        if(DEBUG){
+            error_reporting(-1);
+        }else{
+            error_reporting(0);
+        }
+        set_exception_handler([$this, 'exceptionHandler']);
+    }
+
+    public function exceptionHandler($e){
+        $this->logErrors($e->getMessage(), $e->getFile(), $e->getLine());
+        if($e->getCode() != ''){
+            $this->displayError('Исключение', $e->getMessage(), $e->getFile(), $e->getLine(), $e->getCode());
+        }
+        else{
+            $this->displayError('Исключение', $e->getMessage(), $e->getFile(), $e->getLine());
+        }
+    }
+
+    protected function logErrors($message = '', $file = '', $line = ''){
+        error_log("[". date('Y-m-d H:i:s') ."] Текст ошибки: {$message} | файл: {$file} | Строка: {$line}\n============================\n", 3, ROOT . '/tmp/errors.log');
+    }
+
+    protected function displayError($errno, $errstr, $errfile, $errline, $responce = 404){
+        http_response_code($responce);
+        if($responce == 404 && !DEBUG){
+            require WWW . '/errors/404.php';
+            die;
+        }
+        if(DEBUG){
+            require WWW. '/errors/dev.php';
+        }
+        else{
+            require WWW . '/errors/prod.php';
+        }
+        die;
+    }
+
+}
