d535d0ddce16b7968c84d43dd7f5dd51b60e25ed
Oogie <oogie.webdev@gmail.com>
2018-07-07 21:44:56 +0300

    lesson 7 controller

---
+++ app/controllers/PageController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Oogie
+ * Date: 07.07.2018
+ * Time: 21:43
+ */
+
+namespace app\controllers;
+
+
+class PageController
+{
+
+    public function viewAction(){
+        echo __METHOD__;
+    }
+
+}
