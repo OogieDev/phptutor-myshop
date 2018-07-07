9d23e1f5ecad3f99534e51583aa56eb1e0ae5013
Oogie <oogie.webdev@gmail.com>
2018-07-08 02:18:46 +0300

    last lesson part1 CACHE

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -3,6 +3,8 @@
 namespace app\controllers;
 
 
+use ishop\Cache;
+
 class MainController extends AppController
 {
 
@@ -10,8 +12,17 @@ class MainController extends AppController
         $posts = \R::findAll('test');
         $this->setMeta("Магазин электроники", "Нужна электроника? Тогда тебе к нам!", "Электроника, мобильные телефоны, ноутбуки, планшеты");
         $name = 'Андрей';
+        $names = ['Andrey', 'Jane'];
         $age = 30;
         $pol = 'Мужик';
+        $cache = Cache::instance();
+//        $cache->set('test', $names);
+//        $cache->delete('test');
+        $data = $cache->get('test');
+        if(!$data){
+            $cache->set('test', $names);
+        }
+        debug($data);
         $this->set(compact('name', 'age', 'pol', 'posts'));
     }
 
