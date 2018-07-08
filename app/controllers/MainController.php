89ee5587b3a2a8040e838bc2ebb67a240a438bdd
Oogie <oogie.webdev@gmail.com>
2018-07-08 15:13:14 +0300

    part 2 lesson2 template to framework

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -9,21 +9,7 @@ class MainController extends AppController
 {
 
     public function indexAction(){
-        $posts = \R::findAll('test');
         $this->setMeta("Магазин электроники", "Нужна электроника? Тогда тебе к нам!", "Электроника, мобильные телефоны, ноутбуки, планшеты");
-        $name = 'Андрей';
-        $names = ['Andrey', 'Jane'];
-        $age = 30;
-        $pol = 'Мужик';
-        $cache = Cache::instance();
-//        $cache->set('test', $names);
-//        $cache->delete('test');
-        $data = $cache->get('test');
-        if(!$data){
-            $cache->set('test', $names);
-        }
-        debug($data);
-        $this->set(compact('name', 'age', 'pol', 'posts'));
     }
 
 
